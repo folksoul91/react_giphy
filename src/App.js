c401e486b6775aa5a19fca2fbac48b4b2cfcad73
Dane Kim <mr.dane@danes-mbp.lan>
2022-06-12 15:25:20 -0400

    completed giphy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+import Gif from "./components/Gif";
+import Form from "./components/Form";
+
+export default function App() {
+  const [gifSrc, setGifSrc] = useState({});
+
+  const key = "W9ThL38OlmMnIif0P13v036495Y4OMVA";
+
+  const makeApiCall = async () => {
+    const gifSrc = `https://api.giphy.com/v1/gifs/random?api_key=${key}`;
+    try {
+      const res = await fetch(gifSrc);
+      const json = await res.json();
+      console.log("makeApiCall", json.data);
+      setGifSrc({ image_url: json.data.images.downsized_large.url });
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+
+  useEffect(() => {
+    makeApiCall();
+  }, []);
+
+  const handleSubmit = async (val) => {
+    if (val) {
+      const gifSrc = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${val}&limit=1`;
+      const res = await fetch(gifSrc);
+      const json = await res.json();
+      console.log("handleSumbit", json.data[0].images.downsized_large.url);
+      setGifSrc({ image_url: json.data[0].images.downsized_large.url });
+    } else {
+      makeApiCall();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Giphy</h1>
+      <Form handleSubmit={handleSubmit} />
+      <br />
+      <br />
+      {gifSrc.image_url ? (
+        <Gif gifSrc={gifSrc} />
+      ) : (
+        <h2>Pull random gifs from Giphy</h2>
+      )}
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Gif.js
@@ -0,0 +1,9 @@
+function Gif(props) {
+  return (
+    <div className="gif-row">
+      <img src={props.gifSrc.image_url} alt="" />
+    </div>
+  );
+}
+
+export default Gif;
